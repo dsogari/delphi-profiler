e512acf095704e4316c1e0eaec19e7c562b49bab
Diego Sogari <diego.sogari@gmail.com>
2023-02-02 11:19:51 -0300

    Show elapsed time in client test

--- projects/ProfilerClientTest.dpr
+++ projects/ProfilerClientTest.dpr
@@ -4,37 +4,56 @@ program ProfilerClientTest;
 
 {$R *.res}
 
+
 uses
   System.Classes,
+  System.Diagnostics,
+  System.TimeSpan,
   System.SysUtils,
   Profiler.Trace;
 
+var
+  I: Integer;
+  InnerElapsedUs: Double;
+  OutterElapsedUs: Double;
+
 procedure Inner;
+var
+  Stopwatch: TStopwatch;
 begin
   Trace('Inner');
+  Stopwatch := TStopwatch.StartNew;
   Sleep(100);
+  InnerElapsedUs := InnerElapsedUs + Stopwatch.Elapsed.TotalMilliseconds * 1000;
 end;
 
 procedure Outter;
+var
+  Stopwatch: TStopwatch;
 begin
   Trace('Outter');
+  Stopwatch := TStopwatch.StartNew;
   Sleep(100);
   Inner;
   Sleep(100);
+  OutterElapsedUs := OutterElapsedUs + Stopwatch.Elapsed.TotalMilliseconds * 1000;
 end;
 
-var
-  I: Integer;
+const
+  TotalCalls = 10;
+
 begin
   try
-    Randomize;
-//    SetTracingScopeFilter('O.*');
-    for I := 0 to Random(10) do
+    for I := 1 to TotalCalls do
       Outter;
     SaveTracingProfileToFile('profile.csv');
     SaveTracingStatisticsToFile('stats.csv');
+    Writeln('Outter elapsed (us): ', ((OutterElapsedUs - InnerElapsedUs) / TotalCalls).ToString);
+    Writeln('Inner elapsed (us): ', (InnerElapsedUs / TotalCalls).ToString);
+    ReadLn;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
